5fc63eabebc46e6b0b5a7cb19f3761cebe33373e
katia susan <kcotaquispem@uni.pe>
2017-12-04 10:38:05 -0500

    algo

--- AjusteBondad.m
+++ AjusteBondad.m
@@ -1,6 +1,18 @@
 %asumiendo una distribucion exponencial
-datos=[122, 133, 106, 128, 135, 126];
+datos=[6, 7. 3, 4. 7, 3, 7, 2, 6, 3, 7, 8. 2, 1. 3,5, 8, 7];
+m=8;
 n=length(datos);
+
 %hallemos el Ni
+Ni=zeros(1,8);
 for i=1:n
+  for j=1:m
+    if datos(i)==j
+      Ni(j)++;
+     end
+   end
+   
+end
+
+%distribucion binomial B(8,p) 
   
